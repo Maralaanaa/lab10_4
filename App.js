7133d1b9ea667c9c091ca7d8b7b6eb84bc1d6220
Maralaanaa <gansukh.mapalmaa@gmail.com>
2022-12-08 18:10:44 +0800

    B200910040

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React, {useEffect} from 'react';
+import {View, Animated, StyleSheet} from 'react-native';
+const Loop = () => {
+  const startValue = new Animated.Value(1); 
+  const endValue = 1.5;
+  useEffect(() => {
+    Animated.loop( 
+      Animated.spring(
+        startValue, {
+          toValue: endValue,
+          friction: 1,
+          useNativeDriver: true,
+      }),
+      {iterations: 1000},
+    ).start();
+  }, [startValue, endValue]); 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <View style={styles.container}> 
+      <Animated.View
+      style={[
+        styles.square,
+        {
+    transform: [
+                    {
+                      scale: startValue,
+                    },
+                  ], 
+                },
+              ]} 
+            />
+          </View> 
+        );
+      };
+        const styles = StyleSheet.create({
+          container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+          square: {
+            height: 50,
+            width: 50,
+            backgroundColor: 'red',
+          }, 
+        });
+        export default Loop;
